1f317c0af2a5ec5960062defc4bf2166746c4904
monkeyWangs <2424880409@qq.com>
2017-12-04 16:49:46 +0800

    修改发布环境

--- src/api/doubanApi/instance.js
+++ src/api/doubanApi/instance.js
@@ -4,8 +4,8 @@ import config from '../config';
 const baseUrl = {
   mock: 'https://www.easy-mock.com/mock/5a24d0ef475c043670250893/api',
   development: '/api',
-  prepub: 'http://api.douban.com',
-  production: 'http://api.douban.com'
+  prepub: 'https://api.douban.com',
+  production: 'https://api.douban.com'
 }[config.env || 'mock'];
 
 export default createAPI(baseUrl);
